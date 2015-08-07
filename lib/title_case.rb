9720543f6e2c67b2a19eb6b3fe1a8742d0133c5c
Christoph <christoph.pirringer@gmail.com>
2015-08-07 05:16:28 +0200

    Add functions for multiple words as well as expections for filler-words

--- lib/title_case.rb
+++ lib/title_case.rb
@@ -1,6 +1,17 @@
-class String
+class String	
   define_method(:title_case) do
- 	input_string = self
- 	output_string=input_string.capitalize()
+   input_string = self.split(" ")
+   filler_words = ["and", "the", "of", "or"]
+   counter = 0
+ 	input_string.each() do |word|
+ 		if filler_words.include?(word) && counter != 0 
+ 		  next
+ 		  counter+=1
+ 		else
+ 		  word.capitalize!()
+ 		  counter+=1
+ 		end
+ 	end
+ 	output_string=input_string.join(" ")
   end
 end
--- spec/title_case_spec.rb
+++ spec/title_case_spec.rb
@@ -1,8 +1,21 @@
 require ("rspec")
 require ("title_case")
+require ("pry")
 
 describe ("String#title_case") do
 	it("capitalizes the first letter of the word hello") do
       expect(("hello").title_case()).to(eq("Hello"))
+	end
+
+	it("capitalizes the first letter of the words 'hello girl'") do
+      expect(("hello girl").title_case()).to(eq("Hello Girl"))
+	end
+
+	it("capitalizes all words except filler-words like 'and, the, of, or'") do
+      expect(("hello girl and dog").title_case()).to(eq("Hello Girl and Dog"))
+	end
+
+	it("capitalizes all words except filler-words like 'and, the, of, or', except at the beginning of a sentence.") do
+      expect(("and hello dog of girl").title_case()).to(eq("And Hello Dog of Girl"))
 	end
 end
